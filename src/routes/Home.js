e69fbaa0268e738f597e1ca3878fb21d8de58562
TaeHwan Park <xotj2635@naver.com>
2023-12-14 14:16:31 +0900

    Add build folder

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,11 @@ function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const json = await (await fetch("/clone_MovieWep_project")).json();
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
